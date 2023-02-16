9accfe195f8a201ffda4bd1fbb73cea34e8fd4d7
Anderson-Reges <andersonreges76@gmail.com>
2023-02-16 20:30:27 -0300

    Feat: creating a class character and your implements

---
+++ src/Character.ts
@@ -0,0 +1,105 @@
+import Archetype, { Mage } from './Archetypes';
+import getRandomInt from './utils';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import { Elf, Race } from './Races';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _wisdom: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this.dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this.race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._wisdom = getRandomInt(1, 10);
+    this._energy = {
+      type_: this.archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+  
+  get dexterity(): number {
+    return this._dexterity;
+  }
+  
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this.defense;
+
+    if (damage > 0) this._lifePoints -= damage;
+    if (damage <= 0) this._lifePoints -= 1;
+
+    return this.lifePoints <= 0 ? -1 : this.lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    console.log(`O ${enemy} recebeu ${this.strength} de dano`);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special(enemy: Fighter): void {
+    const specialManaDamage = this._wisdom + this.dexterity;
+    const specialStaminaDamage = this.strength + this.dexterity;
+
+    switch (this.archetype.energyType) {
+      case 'mana':
+        console.log(`O ${enemy} recebeu ${specialManaDamage} de dano`);
+        break;
+      case 'stamina':
+        console.log(`O ${enemy} recebeu ${specialStaminaDamage} de dano`);
+        break;
+      default:
+        break;
+    }
+  }
+}
